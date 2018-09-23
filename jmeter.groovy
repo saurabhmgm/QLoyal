501103daaf3b6d5cd3001685a36e2ace2b509959
saurabhmgm <saurabh.kaushik003@gmail.com>
2018-09-24 09:50:32 +1000

    Jmeter pipeline

---
+++ jmeter.groovy
@@ -0,0 +1,21 @@
+/**
+ * Created by KaushikS on 24/09/2018.
+ */
+pipeline {
+    agent any
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'master', credentialsId: 'saurabh.kaushik003@gmail.com', url: 'https://github.com/saurabhmgm/QLoyal.git'
+
+            }
+        }
+    }
+    stage('Run JMeter Test') {
+        steps {
+            dir('Perf') {
+                bat 'C:\\apache-jmeter-3.3\\bin\\jmeter -n -t PHPTRAVELS.jmx -l PHPTRAVELS.jtl'
+            }
+        }
+    }
+}
