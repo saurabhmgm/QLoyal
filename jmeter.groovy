83cf65841826a951593511605dca0b6171303bc6
saurabhmgm <saurabh.kaushik003@gmail.com>
2018-09-24 11:07:22 +1000

    Update jmeter.groovy

--- jmeter.groovy
+++ jmeter.groovy
@@ -7,12 +7,12 @@ node {
     }
      stage('Run JMeter Test') {
         script {
-        bat 'C:/apache-jmeter-3.3/bin/jmeter -n -t https://github.com/saurabhmgm/QLoyal.git/Perf/PHPTRAVELS.jmx -l PHPTRAVELS.jtl'
+        bat 'C:/apache-jmeter-3.3/bin/jmeter -n -t https://github.com/saurabhmgm/QLoyal.git/Perf/PHPTRAVELS.jmx -l PHPTRAVELS.xml'
       }
     }
      stage("Publish-Results") {
             script{
-                performanceReport parsers: [[$class: 'JMeterParser', glob: 'C:/apache-jmeter-3.3/bin/PHPTRAVELS.jtl']], relativeFailedThresholdNegative: 1.2, relativeFailedThresholdPositive: 1.89, relativeUnstableThresholdNegative: 1.8, relativeUnstableThresholdPositive: 1.5
+                performanceReport parsers: [[$class: 'JMeterParser', glob: 'C:/apache-jmeter-3.3/bin/PHPTRAVELS.xml']], relativeFailedThresholdNegative: 1.2, relativeFailedThresholdPositive: 1.89, relativeUnstableThresholdNegative: 1.8, relativeUnstableThresholdPositive: 1.5
             }
         }
   
