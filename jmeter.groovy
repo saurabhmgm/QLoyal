5da969d6944baeaacd79d1ae91f0fe75ae559b65
saurabhmgm <saurabh.kaushik003@gmail.com>
2018-09-24 11:03:54 +1000

    Jmeter Pipeline Script

--- jmeter.groovy
+++ jmeter.groovy
@@ -2,20 +2,18 @@
  * Created by KaushikS on 24/09/2018.
  */
 node {
-    
-    
-        stage('Checkout') {
-            
-                git branch: 'master', credentialsId: 'saurabh.kaushik003@gmail.com', url: 'https://github.com/saurabhmgm/QLoyal.git'
-
-            
-        }
-    
-    stage('Run JMeter Test') {
+     stage('Checkout') {
+        git branch: 'master', credentialsId: 'saurabh.kaushik003@gmail.com', url: 'https://github.com/saurabhmgm/QLoyal.git'
+    }
+     stage('Run JMeter Test') {
         script {
         bat 'C:/apache-jmeter-3.3/bin/jmeter -n -t https://github.com/saurabhmgm/QLoyal.git/Perf/PHPTRAVELS.jmx -l PHPTRAVELS.jtl'
       }
     }
-   
+     stage("Publish-Results") {
+            script{
+                performanceReport parsers: [[$class: 'JMeterParser', glob: 'C:/apache-jmeter-3.3/bin/PHPTRAVELS.jtl']], relativeFailedThresholdNegative: 1.2, relativeFailedThresholdPositive: 1.89, relativeUnstableThresholdNegative: 1.8, relativeUnstableThresholdPositive: 1.5
+            }
+        }
   
 }
